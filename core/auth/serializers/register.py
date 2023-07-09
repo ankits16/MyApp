88f1f0d9bb288492b60da0e1574d20a2a93c0931
Ankit Sachan <>
2023-07-09 13:38:05 +0530

    configured password length as 4 in Debug mode

--- core/auth/serializers/register.py
+++ core/auth/serializers/register.py
@@ -1,14 +1,15 @@
 from rest_framework import serializers
 from core.user.serializers import UserSerializer
 from core.user.models import User
+from django.conf import settings
 
 class RegisterSerializer(UserSerializer):
     """User resister serializer"""
     # make sure password is aleast 8 characters
     #no loger that 128 characters
     #cannot be read by the user
-
-    password = serializers.CharField(max_length=128, min_length=8, write_only=True, required=True)
+    password_min_length = 4 if settings.DEBUG else 8
+    password = serializers.CharField(max_length=128, min_length=password_min_length, write_only=True, required=True)
 
     class Meta:
         model = User
@@ -17,4 +18,4 @@ class RegisterSerializer(UserSerializer):
         fields = ['id', 'username', 'email', 'password', 'first_name', 'last_name']
 
     def create(self, validated_data):
-        return User.objects.create(**validated_data)
+        return User.objects.create_user(**validated_data)
