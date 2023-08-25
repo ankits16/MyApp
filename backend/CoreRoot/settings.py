bf323af8c5c5f3ecde7e38afae3de697d366b0e0
Ankit Sachan <>
2023-08-25 18:37:58 +0530

    updated

--- backend/CoreRoot/settings.py
+++ backend/CoreRoot/settings.py
@@ -212,8 +212,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
-EMAIL_HOST_USER = 'ankit.sachan16@gmail.com'
-EMAIL_HOST_PASSWORD = 'muxwlbqhhgimyont'
+EMAIL_HOST_USER = 'ttt'
+EMAIL_HOST_PASSWORD = 'ttt'
 
 #swagger
 SWAGGER_SETTINGS = {
